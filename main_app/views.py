6cf3ab7221ec14c717bc8d63cdbb1bf0981e3bf3
reddy <reddyfede@gmail.com>
2023-09-20 22:04:13 +0200

    create home, about, books/index routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+books = [
+    {'title': 'Moby Dick', 'author': 'Herman Melville', 'year':1851, 'description':'A book about an old man that likes fishing.'},
+    {'title': 'Frankestein', 'author': 'Mary Shelley', 'year':1818, 'description':'A book about a monster not called Frankestein.'},
+    {'title': 'Dracula', 'author': 'Bram Stoker', 'year':1897, 'description':'There\'s almost no Dracula in here.'},
+    {'title': 'The Colour Out of Space', 'author': 'H.P. Lovecraft', 'year':1927, 'description':'A short story about color blindness.'},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def books_index(request):
+    return render(request, 'books/index.html', {
+        'books': books
+    })
