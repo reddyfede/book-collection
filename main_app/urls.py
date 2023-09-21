1447b36a286dc17704ee4d5ca8b1a5f53f3a1343
reddy <reddyfede@gmail.com>
2023-09-21 12:57:40 +0200

    add book detail url,view,template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('books/', views.books_index, name='books_index')
+    path('books/', views.books_index, name='index'),
+    path('books/<int:book_id>/', views.books_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,15 +3,6 @@ from .models import Book
 
 # Create your views here.
 
-# books = [
-#     {'title': 'Moby Dick', 'author': 'Herman Melville', 'year':1851, 'description':'A book about an old man that likes fishing.'},
-#     {'title': 'Frankestein', 'author': 'Mary Shelley', 'year':1818, 'description':'A book about a monster not called Frankestein.'},
-#     {'title': 'Dracula', 'author': 'Bram Stoker', 'year':1897, 'description':'There\'s almost no Dracula in here.'},
-#     {'title': 'The Colour Out of Space', 'author': 'H.P. Lovecraft', 'year':1927, 'description':'A short story about color blindness.'},
-# ]
-
-# b = Book(title= 'Moby Dick',author= 'Herman Melville', year=1851, description='A book about an old man that likes fishing.')
-
 def home(request):
     return render(request, 'home.html')
 
@@ -22,4 +13,10 @@ def books_index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {
         'books': books
+    })
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id = book_id)
+    return render(request, 'books/detail.html', {
+        'book': book
     })
