03590e76bc03cc95f6daf8ca5b4dbc58e2313c94
reddy <reddyfede@gmail.com>
2023-09-22 12:53:24 +0200

    add quote model

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,13 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.id})
+    
+class Quote(models.Model):
+    text = models.TextField(max_length=300)
+    book = models.ForeignKey(
+        Book,
+        on_delete = models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.text}'
