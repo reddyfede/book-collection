e53aaf11a1293630e83220dd9e613212b3c632de
reddy <reddyfede@gmail.com>
2023-09-22 20:00:54 +0200

    add M:M relation book-genre

--- main_app/models.py
+++ main_app/models.py
@@ -9,12 +9,12 @@ class Genre(models.Model):
     def __str__(self):
         return f'{self.name} - {self.id}'
 
-
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
     description = models.TextField(max_length=300)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return f'{self.title} - {self.id}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
-from .models import Book
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from .models import Book, Genre
 from .forms import QuoteForm
 
 # Create your views here.
@@ -14,9 +14,12 @@ def about(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id = book_id)
     quote_form = QuoteForm()
+    genre_list = book.genres.all().values_list('id')
+    not_genres = Genre.objects.exclude(id__in = genre_list)
     return render(request, 'main_app/book_detail.html',{
         'book': book,
         'quote_form': quote_form,
+        'not_genres': not_genres,
     })
 
 class BooksList(ListView):
@@ -25,7 +28,7 @@ class BooksList(ListView):
 
 class BookAdd(CreateView):
     model = Book
-    fields = '__all__'
+    fields = ['title','author','year','description']
 
 class BookEdit(UpdateView):
     model = Book
